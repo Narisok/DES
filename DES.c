4f32a336d1fbb0ec082cb8b7b363b42e3b057379
Narisok <dimedrov123@gmail.com>
2021-03-03 13:10:35 +0200

    des encoding decoding and keygeneration

--- DES.c
+++ DES.c
@@ -14,11 +14,20 @@
     }                                                                                      \
 } 
 
-#define DIVIDE_TWO(block, left, right, size) {   \
-    left = 0x0;                                  \
-    right = 0x0;                                 \
-    left = block >> (size) & ~(~1<<((size)-1));  \
-    right = block & ~(~1<<((size)-1));           \
+#define DIVIDE_TWO(block, left, right, size) {      \
+    left = 0x0;                                     \
+    right = 0x0;                                    \
+    left = block >> (size);                         \
+    right = block;                                  \
+}
+
+#define DIVIDE_TWO_CLEAR(block, left, right, size) {  \
+    left = 0x0;                                       \
+    right = 0x0;                                      \
+    left = block >> (size);                           \
+    left = ~(~(left&0)<<(size)) & left;               \
+    right = block;                                    \
+    right = ~(~(right&0)<<(size)) & right;            \
 }
 
 #define LEFT_CYCLE_SHIFT(block, block_size, shift) {                                                              \
@@ -64,6 +73,78 @@ static const uint8_t PC2_table[48] = {
     51, 45, 33, 48, 44, 49,  39, 56, 34, 53, 46, 42, 50, 36, 29, 32
 };
 
+static const uint8_t E_table[48] = {
+    32,     1,  2,  3,  4,      5,
+    4,      5,  6,  7,  8,      9,
+    8,      9,  10, 11, 12,     13,
+    12,     13, 14, 15, 16,     17,
+    16,     17, 18, 19, 20,     21,
+    20,     21, 22, 23, 24,     25,
+    24,     25, 26, 27, 28,     29,
+    28,     29, 30, 31, 32,     1
+};
+
+
+static const uint8_t S_table[8][4][16] = {
+    {
+        {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+        {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+        {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+        {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
+    },
+    {
+        {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+        {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+        {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+        {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
+    },
+    {
+        {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+        {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+        {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+        {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
+    },
+   { 
+        {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
+        {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
+        {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
+        {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 }
+    },
+    {
+        {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
+        {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
+        {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
+        {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 }
+    },
+    {
+        {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
+        {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+        {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
+        {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 }
+    },
+    {
+        {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
+        {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+        {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+        {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 }
+    },
+    {
+        {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
+        {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+        {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
+        {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
+    }
+};
+
+static const uint32_t P_table[32] = {
+    16, 7, 20, 21, 
+    29, 12, 28, 17,
+    1, 15, 23, 26, 5, 18, 31, 10,
+    2, 8, 24, 14, 32, 27, 3, 9,
+    19, 13, 30, 6, 22, 11, 4, 25
+};
+
+
 //=============================> DES keys generation <========================//
 
 static inline uint64_t PC1(uint64_t key64b);
@@ -74,7 +155,7 @@ void DES_keys_generate(uint64_t key, uint64_t keys48b[16])
     uint64_t key56b = PC1(key);
 
     uint32_t C,D;
-    DIVIDE_TWO(key56b,C,D,28);
+    DIVIDE_TWO_CLEAR(key56b,C,D,28);
 
     for(size_t i = 0; i < 16; ++i) {
         LEFT_CYCLE_SHIFT(C,28,SHIFT_table[i])
@@ -90,16 +171,46 @@ void DES_keys_generate(uint64_t key, uint64_t keys48b[16])
 
 //=============================> DES one block encode <========================//
 
-static uint64_t InitialPermutation(uint64_t block);
-static uint64_t InitialPermutationReverse(uint64_t block);
-
-
-// static void FeistelRound(uint32_t *L, uint32_t *R, uint64_t key48b);
-// static uint32_t F(uint32_t block, uint64_t key48b);
+static inline uint64_t IP(uint64_t block);
+static inline uint64_t IP_reverse(uint64_t block);
+static inline uint32_t F(uint32_t Ri, uint64_t key48b);
 
 uint64_t DES_encode_block(uint64_t  block, uint64_t keys48b[16])
 {
-    return 0x0;
+    block = IP(block);
+    uint32_t Li, Ri;
+    DIVIDE_TWO(block, Li, Ri, 32)
+
+    for(unsigned i = 0; i < 16; ++i) {
+        uint32_t Li_plus = Ri;
+        uint32_t Ri_plus = Li ^ F(Ri, keys48b[i]);
+        Li = Li_plus;
+        Ri = Ri_plus;
+    }
+
+    COMBINE_TWO(Ri, Li, block, 32)
+    block = IP_reverse(block);
+
+    return block;
+}
+
+uint64_t DES_decode_block(uint64_t block, uint64_t keys48b[16])
+{
+    block = IP(block);
+
+    uint32_t Li, Ri;
+    DIVIDE_TWO(block, Li, Ri, 32)
+
+    for(unsigned i = 0; i < 16; ++i) {
+        uint32_t Li_minus = Ri;
+        uint32_t Ri_minus = Li ^ F(Ri, keys48b[15-i]);
+        Li = Li_minus;
+        Ri = Ri_minus;
+    }
+
+    COMBINE_TWO(Ri, Li, block, 32)
+    block = IP_reverse(block);
+    return block;
 }
 
 
@@ -121,16 +232,61 @@ static inline uint64_t PC2(uint64_t CiDi)
 
 
 
-static uint64_t InitialPermutation(uint64_t block)
+static inline uint64_t IP(uint64_t block)
 {
     uint64_t newBlock;
     PERMUTATION(block, newBlock, 64, IP_table)
     return newBlock;
 }
 
-static uint64_t InitialPermutationReverse(uint64_t block)
+static inline uint64_t IP_reverse(uint64_t block)
 {
     uint64_t newBlock;
     PERMUTATION(block, newBlock, 64, IP_table_reverse)
     return newBlock;
+}
+
+
+static inline uint64_t E(uint32_t Ri);
+static inline uint32_t S(uint64_t B8);
+static inline uint32_t P(uint32_t raw);
+
+static inline uint32_t F(uint32_t Ri, uint64_t key48b)
+{
+    uint64_t exp48b = E(Ri);
+    uint64_t B8     = exp48b ^ key48b;
+    uint32_t result = S(B8);
+    return            P(result);
+}
+
+static inline uint64_t E(uint32_t Ri)
+{
+    uint64_t exp48b = 0x0;
+    for(size_t i = 0; i < 48; ++i) {            
+        exp48b |= (uint64_t)(Ri >> (32-E_table[i]) & 1 )  << (47-i);         
+    }
+    return exp48b;
+}
+
+static inline uint32_t S(uint64_t B8)
+{
+    uint32_t res = 0x0;
+    for(int i = 0; i < 8; ++i) {
+        uint8_t Bi = B8 >> (42 - i*6) & 0b00111111;
+
+        uint8_t row = (Bi>>4&2) | (Bi&1) ;
+        uint8_t col = (Bi>>1)&0xf;
+        uint32_t S = S_table[i][row][col];
+       
+
+        res |= S << (28 - i *4);
+    }
+    return res;
+}
+
+static inline uint32_t P(uint32_t raw)
+{
+    uint32_t res;
+    PERMUTATION(raw, res, 32, P_table)
+    return res;
 }
--- DES.h
+++ DES.h
@@ -1,6 +1,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 /**
  * @brief 
  * 
@@ -17,3 +21,16 @@ void DES_keys_generate(uint64_t key, uint64_t keys48b[16]);
  * @return uint64_t 
  */
 uint64_t DES_encode_block(uint64_t  block, uint64_t keys48b[16]);
+
+/**
+ * @brief 
+ * 
+ * @param block 
+ * @param keys48b 
+ * @return uint64_t 
+ */
+uint64_t DES_decode_block(uint64_t block, uint64_t keys48b[16]);
+
+#ifdef __cplusplus
+}
+#endif
