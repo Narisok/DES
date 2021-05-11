f40b0fbfcd2044ba43514f28b697c278f89de87c
Andrew <Dimedrov123@gmail.com>
2021-05-11 23:07:14 +0300

    upd shell scripts

--- examples/code and check.sh
+++ examples/code and check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bash ./examples/build_utility.sh
-echo "Utility:" $(time echo $1 | ./Build/Examples/utility | ./Build/Examples/utility -d)
+echo "Utility:" $(time echo $1 | ./Build/Examples/utility -K 1234567812345678 | ./Build/Examples/utility -K 1234567812345678 -d)
 echo "OpenSSL:" $(time echo $1 | openssl des-ecb -K 1234567812345678 | openssl des-ecb -d -K 1234567812345678)
-echo "Check:" $(time echo $1 | openssl des-ecb -K 1234567812345678 | ./Build/Examples/utility -d)
-echo "Check:" $(time echo $1 | ./Build/Examples/utility | openssl des-ecb -d -K 1234567812345678)
+echo "Check:" $(time echo $1 | openssl des-ecb -K 1234567812345678 | ./Build/Examples/utility -d -K 1234567812345678)
+echo "Check:" $(time echo $1 | ./Build/Examples/utility -K 1234567812345678 | openssl des-ecb -d -K 1234567812345678)
--- examples/run_benchmark.sh
+++ examples/run_benchmark.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 bash ./examples/build_utility_prod.sh
+echo -e "\e[32m\e[1mEncrypting...\e[0m"
+echo -ne "TIME\tOpenSSL:" 
+$(time cat my_file.txt |  openssl des-ecb -K 1234567812345678  > openssl_encrypt.enc)
+echo -ne "\nTIME\tUtility:" 
+$(time cat my_file.txt | ./Build/Examples/utility -K 1234567812345678 > util_encrypt.enc)
 
-echo "OpenSSL:" $(time cat my_file.txt |  openssl des-ecb -K 1234567812345678  > openssl_encrypt.enc)
-echo "Utility:" $(time cat my_file.txt | ./Build/Examples/utility > util_encrypt.enc)
-
-# echo "Utility:" $(time echo $1 | ./Build/Examples/utility | ./Build/Examples/utility -d)
-# echo "OpenSSL:" $(time echo $1 | openssl des-ecb -K 1234567812345678 | openssl des-ecb -d -K 1234567812345678)
+echo -e "\n\e[32m\e[1mDecrypting...\e[0m"
+echo -ne "TIME\tOpenSSL:"
+ $(time cat openssl_encrypt.enc |  openssl des-ecb -K 1234567812345678  > openssl_encrypt.dec)
+echo -ne "\nTIME\tUtility:" 
+$(time cat util_encrypt.enc | ./Build/Examples/utility -K 1234567812345678 > util_encrypt.dec)
