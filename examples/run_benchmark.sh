4c78d91398475541f6ec76eb637278e878012932
Andrew the Cat <dimedrov123@gmail.com>
2021-05-12 22:45:30 +0300

    fix benchmark coloring

--- examples/run_benchmark.sh
+++ examples/run_benchmark.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 bash ./examples/build_utility_prod.sh
-echo -e "\e[32m\e[1mEncrypting...\e[0m"
+echo -e "\033[32m\033[1mEncrypting...\033[0m"
 echo -ne "TIME\tOpenSSL:" 
 $(time cat my_file.txt |  openssl des-ecb -K 1234567812345678  > openssl_encrypt.enc)
 echo -ne "\nTIME\tUtility:" 
 $(time cat my_file.txt | ./Build/Examples/utility -K 1234567812345678 > util_encrypt.enc)
 
-echo -e "\n\e[32m\e[1mDecrypting...\e[0m"
+echo -e "\n\033[32m\033[1mDecrypting...\033[0m"
 echo -ne "TIME\tOpenSSL:"
  $(time cat openssl_encrypt.enc |  openssl des-ecb -K 1234567812345678  > openssl_encrypt.dec)
 echo -ne "\nTIME\tUtility:" 
